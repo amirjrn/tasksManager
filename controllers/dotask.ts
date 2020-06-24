e62099259d85a21dfe2dbbea7d60c35b9bd06153
amir jiryan <amirjiryan@gmail.com>
2020-06-24 17:38:10 +0430

    added do task support

--- controllers/dotask.ts
+++ controllers/dotask.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
 import { tasksUseCases } from '../use-cases/index'
 export default function () {
-  return async (req: Request, res: Response) => {}
+  return async (req: Request, res: Response) => {
+    const dotaskResult = await tasksUseCases.doTask({ userId: req.user._userId, taskId: req.body.taskId })
+    dotaskResult ? res.status(201).json({ message: 'انجام تسک با موفقیت ثبت شد' }) : res.status(400).json({ message: 'اضافه کردن تسک با موفقیت انجام نشد' })
+  }
 }
