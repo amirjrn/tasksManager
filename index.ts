388a0c975b360ea5f7539add4579969f476faa23
amir jiryan <amirjiryan@gmail.com>
2020-06-28 15:49:20 +0430

    fix react app bug

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express'
+import * as path from 'path'
 import taskController from './controllers/task'
 import * as bodyParser from 'body-parser'
 import * as cookieParser from 'cookie-parser'
@@ -17,8 +18,6 @@ config()
 const port = process.env.PORT || 4000
 const app = express()
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
-
 app.use(express.static('public'))
 
 app.use(cookieParser())
@@ -30,18 +29,18 @@ app.use(Passport.initialize())
 // middlewares to parse get method urls and post method's json
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.post('/api/task', taskController())
+app.post('/api/dotask', Passport.authenticate('jwt', { session: false }), dotaskController())
 
-app.post('/task', taskController())
-app.post('/dotask', Passport.authenticate('jwt', { session: false }), dotaskController())
-app.get('/', (req, res) => {
-  res.send('index.html')
+app.post('/api/register', registerContorller())
+app.post('/api/login', loginController(), entrance())
+app.post('/api/logout', Passport.authenticate('jwt', { session: false }), logoutController(), exit())
+
+app.get('/api/tasks', Passport.authenticate('jwt', { session: false }), tasksContorller())
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
 })
-
-app.post('/register', registerContorller())
-app.post('/login', loginController(), entrance())
-app.post('/logout', Passport.authenticate('jwt', { session: false }), logoutController(), exit())
-
-app.get('/tasks', Passport.authenticate('jwt', { session: false }), tasksContorller())
 
 app.listen(port, function () {
   console.log(`server listening on port ${port}`)
